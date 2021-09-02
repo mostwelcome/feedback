224d2929beb85396c52e118236e49b2ea26aaeb2
mostwelcome <tuadutta13@gmail.com>
2021-09-02 09:41:28 +0530

    added profiles into database

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from .models import UserProfile
+from django.views.generic.edit import CreateView
+
+from .models import UserProfile
 
 
-# Create your views here.
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    fields = '__all__'
+    model = UserProfile
+    success_url = '/profile'
 
-
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
-
-    def post(self, request):
-        print(request.FILES['image'])
-        return HttpResponseRedirect('/profile')
-
+#
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             'form': form
+#         })
+#
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+#
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES['user_image'])
+#             profile.save()
+#             return HttpResponseRedirect('/profile')
+#         return render(request, "profiles/create_profile.html")
